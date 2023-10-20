e84f757870d5778223d022c95077c43ddd681ba4
mutecross <mutecross@gmail.com>
2023-10-20 23:17:23 +0300

    создан модуль добавления фотографий на главную страницу

---
+++ js/picture.js
@@ -0,0 +1,31 @@
+import {createArrayPhotos} from './data.js';
+
+//поиск шаблона и блока ссылки внутри него
+const similarPhotoTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+//создание фрагмента
+const similarListFragment = document.createDocumentFragment();
+
+//формирование массива с фотографиями
+const arrayPhotos = createArrayPhotos(25);
+arrayPhotos.forEach(({url, description, likes, comments}) => {
+  //клонирование блока ссылки из template
+  const photoElement = similarPhotoTemplate.cloneNode(true);
+  //вставка атрибутов в фотографию
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__img').alt = description;
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__comments').textContent = comments.length;
+  //вставка фотографии во фрагмент
+  similarListFragment.appendChild(photoElement);
+});
+
+//добавление фрагмента в финальный контейнер для фотографий
+const addPhotosFragment = function () {
+  const finalListPhotos = document.querySelector('.pictures');
+  finalListPhotos.appendChild(similarListFragment);
+};
+
+export {addPhotosFragment};
