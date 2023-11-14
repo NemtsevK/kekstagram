c83ad21418d52fcef501b3b3a4ff78db1725764d
Konstantin <mutecross@gmail.com>
2023-11-14 21:50:43 +0300

    удалены лишнии функции и добавлены новые от кекса

--- js/util.js
+++ js/util.js
@@ -1,32 +1,43 @@
-//Получить случайное число в диапазоне
-const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-
-//Получить случайный элемент массива
-const getArrayRandElement = (elements) => elements[getRandomInt(0, elements.length - 1)];
-
-const createArray = (start, end) => {
-  const array = [];
-  let count = 0;
-  for (let i = start; i <= end; i++) {
-    array[count] = i;
-    count++;
-  }
-  return array;
-};
-
-//перемешать массив (чтобы при выборе случайных элементов не было повторений)
-const shuffleArray = (array) => {
-  let j, temp;
-  for (let i = array.length - 1; i > 0; i--) {
-    j = Math.floor(Math.random() * (i + 1));
-    temp = array[j];
-    array[j] = array[i];
-    array[i] = temp;
-  }
-  return array;
-};
-
 //если нажата кнопка Esc
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-export {getRandomInt, getArrayRandElement, shuffleArray, createArray, isEscapeKey};
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  // Используем замыкания, чтобы время "последнего кадра" навсегда приклеилось
+  // к возвращаемой функции с условием, тогда мы его сможем перезаписывать
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export {isEscapeKey, debounce, throttle};
